6d863ba5baeae6c3e204c35c1eb61c79d3d67923
EvanAdhiguna <evanadhiguna05@gmail.com>
2025-07-02 22:37:20 +0800

    memperbaiki string

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.gridlayout.gridlayout)
+    implementation(libs.androidx.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
